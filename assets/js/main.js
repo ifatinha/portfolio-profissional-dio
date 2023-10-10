89fad76965b7c5355456a68a2ed18930c7f155c9
ifatinha <ifatinha99@gmail.com>
2023-10-10 20:25:29 -0300

    Finish project

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,14 +21,66 @@ function resolveProfileInfo(profileData) {
     mail.href = `mailto:${profileData.email}`;
 }
 
-function resolverHardSkills(profileData) {
+function resolverSoftSkills(profileData) {
     const softSkills = document.querySelector("#softSkills");
-    console.log(softSkills);
-    
+    softSkills.innerHTML = profileData.skills.softSkills.map((skill) => {
+        return `<li>${skill}</li>`
+    }).join("");
+}
+
+function resolverHardSkills(profileData) {
+    const hardSkills = document.querySelector("#hardSkills");
+    hardSkills.innerHTML = profileData.skills.hardSkills.map((skill) => {
+        return `<li>
+                    <a href="https://www.flaticon.com/br/icones-gratis/html" title="html ícones">
+                    <img src="${skill.logo}" alt="${skill.name}" title="${skill.name}">
+                </a>
+            </li>`
+    }).join("");
+}
+
+function resolverProfileLanguages(profileData) {
+    const languages = document.querySelector("#languages");
+    languages.innerHTML = profileData.languages.map((language) => {
+        return `<li>${language}</li>`
+    }).join("");
+}
+
+function resolveProjects(profileData) {
+    const projects = document.querySelector("#projects");
+    projects.innerHTML = profileData.portfolio.map((item) => {
+        return `<li>
+                    <h2>
+                        <i class="fa-brands fa-github"></i>
+                        ${item.name}
+                    </h2>
+                    <a href="${item.url}" title="${item.name}">
+                        ${item.url}
+                    </a>
+                 </li>`
+    }).join("");
+}
+
+function resolveExperience(profileData) {
+    const experience = document.querySelector("#experience");
+    experience.innerHTML = profileData.professionalExperience.map((experience) => {
+        return `<li>
+                    <h3>${experience.name}</h3>
+                    <span>
+                        <i class="fa-solid fa-calendar-days"></i>
+                        ${experience.period}
+                    </span>
+                    <p>${experience.description}</p>
+                </li>`
+    }).join("");
 }
 
 (async () => {
     const profileData = await fetchProfileDate();
     resolveProfileInfo(profileData);
+    resolverSoftSkills(profileData);
     resolverHardSkills(profileData);
+    resolverProfileLanguages(profileData);
+    resolveProjects(profileData);
+    resolveExperience(profileData);
 })()
