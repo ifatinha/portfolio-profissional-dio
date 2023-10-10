852c7a700a3780aea16c8886bcc80c41b5603f08
ifatinha <ifatinha99@gmail.com>
2023-10-10 20:33:28 -0300

    update

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileDate() {
-    const url = "/data/profile.json";
+    const url = "data/profile.json";
     const fetching = await fetch(url);
     return await fetching.json();
 }
